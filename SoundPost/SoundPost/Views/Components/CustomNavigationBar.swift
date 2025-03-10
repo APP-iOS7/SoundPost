4a4e8039dda9a9c182bad8c9bf7d36c24ca65066
AIsiteru98 <dlwodyd0517@gmail.com>
2025-03-10 16:28:37 +0900

    customnavigationbar componnent

---
+++ SoundPost/SoundPost/Views/Components/CustomNavigationBar.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+
+struct CustomNavigationBar: View {
+    private let title: String
+    
+    private let btnInits: [(title: String, action: () -> Void)]
+    
+    init (title: String, btnInits: [(title: String, action: () -> Void)] = []) {
+        self.title = title
+        self.btnInits = btnInits
+    }
+    var body: some View {
+        ZStack {
+            HStack {
+                Spacer()
+                Text(title)
+                    .font(.headline)
+                Spacer()
+            }
+            HStack {
+                Spacer()
+                ForEach(btnInits, id: \.0) { btnInit in
+                    Button(action: btnInit.action) {
+                        if btnInit.title == btnInits.last?.title {
+                            Text(btnInit.title)
+                                .padding(.trailing)
+                        } else {
+                            Text(btnInit.title)
+                        }
+                        
+                    }
+                }
+            }
+        }
+        .frame(height: 30)
+    }
+}
---
+++ SoundPost/SoundPost/Views/ProfileView/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SoundPost
+//
+//  Created by 이재용 on 3/10/25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
