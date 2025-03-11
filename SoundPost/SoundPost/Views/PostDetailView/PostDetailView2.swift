a49ff6995ec9223c976b5e8a07e695b6f149e665
hajinCHOI <cresent0203@gmail.com>
2025-03-11 11:09:03 +0900

    commentButton 모달 생성

--- SoundPost/SoundPost/Views/PostDetailView/PostDetailView2.swift
+++ SoundPost/SoundPost/Views/PostDetailView/PostDetailView2.swift
@@ -2,7 +2,13 @@ import SwiftUI
 
 struct PostDetailView2: View {
     @ObservedObject var post: PostViewModel
+    @StateObject private var quickStartViewModel : QuickStartButtonViewModel
     @State private var comments: [CommentViewModel] = []
+    @State var isShowingSheet: Bool = false
+    init(post: PostViewModel, quickStartViewModel : QuickStartButtonViewModel) {
+        self.post = post
+        self._quickStartViewModel = StateObject(wrappedValue: quickStartViewModel)
+    }
     
     var body: some View {
         ScrollView {
@@ -29,9 +35,14 @@ struct PostDetailView2: View {
                 .padding(.top)
             }
         }
+        .sheet(isPresented: $isShowingSheet) {
+                    ModalContentView()
+                .presentationDetents([.height(UIScreen.main.bounds.height / 7)])
+                }
         .safeAreaInset(edge: .bottom, spacing: 0) {
             // 하단에 고정된 대답하기 버튼
             Button {
+                isShowingSheet.toggle()
                 // TODO: 대답하기 버튼 액션
                 // 녹음 시작
                 // 녹음 완료
@@ -51,6 +62,49 @@ struct PostDetailView2: View {
     }
 }
 
+struct ModalContentView: View {
+    @ObservedObject private var commentButtonViewModel: ContentViewModel = ContentViewModel()
+    var body: some View {
+        VStack {
+            Button {
+                commentButtonViewModel.QuickStartSet()
+            } label: {
+                VStack {
+                    switch commentButtonViewModel.QuickStartButtonClick {
+                    case 1:
+                        Image(systemName: "record.circle")
+                            .foregroundColor(.red)
+                            .font(.title)
+                        Text("녹음")
+                            .foregroundColor(.red)
+                            .font(.caption)
+                    case 2 :
+                        Image(systemName: "stop.circle.fill")
+                            .foregroundColor(.red)
+                            .font(.title)
+                        Text("녹음중")
+                            .foregroundColor(.red)
+                            .font(.caption)
+                    case 3 :
+                        Image(systemName: "record.circle")
+                            .foregroundColor(.red)
+                            .font(.title)
+                        Text("재녹음")
+                            .foregroundColor(.red)
+                            .font(.caption)
+                    default:
+                        Image(systemName: "plus.circle.fill")
+                            .foregroundColor(commentButtonViewModel.QuickStartButtonColor())
+                            .font(.title)
+                        Text("포스팅")
+                            .foregroundColor(commentButtonViewModel.QuickStartButtonColor())
+                            .font(.caption)
+                    }
+                }
+            }
+        }
+    }
+}
 //#Preview {
 //    PostDetailView(post: PostViewModel.createPreview())
 //}
